ad48b0368aad1a480d2fa41a469dc210665be76d
Himanshu <mishrahimanshu070@gmail.com>
2021-12-27 16:52:05 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ export default function Home() {
     }
   return (
       <>
-      <Head><link rel="preconnect" href="https://fonts.googleapis.com"/>
+      {/* <Head><link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@800&display=swap" rel="stylesheet"/> </Head>
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@800&display=swap" rel="stylesheet"/> </Head> */}
 
 
         {/* <div className={` ${styles.mishra} `}>sdjfl;kasdjflkjds</div> */}
