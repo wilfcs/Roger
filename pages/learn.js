61b11262cc943220dd077b5d841ed46076f06559
Himanshu <mishrahimanshu070@gmail.com>
2021-12-28 00:21:53 +0530

    added image

---
+++ pages/learn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import morse from '../public/images/wp5048666.png'
+import Image from 'next/image';
+function Header() {
+    return (
+        <Image src="/images/wp5048666.png" width={100} height={100} alt="Morse Code" />
+        
+    )
+}
+
+export default Header;
