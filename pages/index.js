7990030bbc396a50f045a7278aa2a7404fc67d74
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2021-12-27 17:38:14 +0530

    Dot value updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 
@@ -8,6 +9,47 @@ export default function Home() {
     var audio = document.getElementById('a1');
     audio.play();
     }
+
+    // Fucntion for updating Input Values
+    const [val, setVal] = useState(".");
+    const [time, setTime] = useState("0");
+    
+
+    let startClick =  0;
+    const click =  (e)=>{
+      const d = new Date();
+      let s = (d.getSeconds()); 
+      if( parseInt(s)- parseInt(time) >2){
+          setVal(`${val} / `)
+          setTime(s);
+  
+      }
+  
+        if(e.type == "mousedown"){
+  
+          startClick = e.timeStamp;
+        }
+        else if(e.type == "mouseup" && startClick > 0){
+          if(e.timeStamp - startClick > 500){  // 0.5 secound
+              setVal(`${val} -`)
+              const d = new Date();
+              let s = (d.getSeconds()); 
+              setTime(s);
+  
+          }
+          else{
+            const d = new Date();
+
+            let s = (d.getSeconds()); 
+              setTime(s);
+              setVal(`${val} .`)
+  
+          }
+        }
+  
+      
+    }
+    // The end of Function 
   return (
       <>
       {/* <Head><link rel="preconnect" href="https://fonts.googleapis.com"/>
@@ -21,13 +63,13 @@ export default function Home() {
             <div className={styles.logo}></div>
             <div className={styles.brandName}>Roger</div>
             
-            <input type="text" className={styles.showMorse}/>
-            <input type="text" className={styles.showText}/>
+            <input type="text" className={styles.showMorse} value={val} />
+            <input type="text" className={styles.showText}   />
           </div>
 
         <div className={styles.body}>
           {/* <button className={` ${styles.logo} ${styles.convex}` }></button> */}
-          <div className={styles.bigButton} id='bigButton' onClick={play}></div>
+          <div className={styles.bigButton} id='bigButton' onMouseDown={click} onMouseUp={click}></div>
           <audio id='a1'>
           <source src='/sound/bottonpress.wav' type='audio/mpeg' />
           Your browser does not support the audio element.
