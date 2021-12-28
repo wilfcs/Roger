05b99fbda729812f30b6fad2a5dd390253f8e74e
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2021-12-28 21:56:24 +0530

    Profile Card Height Changed

--- components/DeveloperLandingCard.js
+++ components/DeveloperLandingCard.js
@@ -36,21 +36,21 @@ const DeveloperLandingCard = (props) => {
           {/* <Image src="/Images/Frame.png" alt="logo" width={22} height={22} /> */}
           </div></div>
         <div className="text-lg mt-4 font-normal">{props.qual}</div>
-        <div className="text-lg mt-4 font-bold text-blue-600">{props.tech}</div>
+        <div className="text-lg mt-4 font-bold text-gray-600">{props.tech}</div>
       </div>
       {/* <Link href="/developers" className="button"> */}
       <Link href={props.githubUrl?props.githubUrl:""} >
       <a target="_blank" className="button">
-        <span className="pl-16 font-bold text-blue-600">Check Profile</span>
+        <span className="pl-7 font-bold text-gray-600">Check Profile</span>
         <span className="arrow">
-            <Image src="/Images/Arrow.png" alt="logo" width={15} height={15} />
+            <Image src="/images/Arrow.png" alt="logo" width={15} height={15} />
         </span>
       </a>
       </Link>
       {/* </Link> */}
       <style jsx>{`
         .cardBody{
-                height: 360px;
+                height: 397px;
         width: 267px;
         background-color: #c4c4cd;
         box-shadow: 12px 12px 24px 0 rgba(0, 0, 0, 0.2), -12px -12px 24px 0 rgba(255, 255, 255, 0.5);
@@ -83,7 +83,7 @@ const DeveloperLandingCard = (props) => {
         }
         
         .button{
-          width: 250px;
+          width: 200;
           height: 55px;
           margin-top: 20px;
           background-color: #c4c4cd;
